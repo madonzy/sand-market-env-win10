10fe2b84647dbb86b7e1c6c9225bc42c092f14ac
Oleksandr Yeremenko <oyeremenko@divante.pl>
2017-06-30 11:04:49 +0200

    Fix folder sharings. Add functionality to change magento2 unix password

--- m2devbox-init.bat
+++ m2devbox-init.bat
@@ -2,10 +2,13 @@
 ECHO Build docker images
 docker-compose up --build -d
 
+ECHO Setting new magento2 linux user password
+docker-compose exec web passwd magento2
+
 ECHO Copy Webroot
 XCOPY "./shared/webroot\*.*" "magento2" /E /S /I
 docker cp magento2 sand_market_box_web:/var/www
-docker exec -it sand_market_box_web chown -R magento2:magento2 /var/www/magento2
+docker exec -it sand_market_box_web chown -R magento2:magento2 /home/magento2/magento2
 RMDIR magento2 /S /Q
 TIMEOUT 5
 
@@ -28,20 +31,20 @@ IF %SAMPLE_DATA_FOLDER_EMPTY% == "false" do
 	ECHO Copy Sample Data modules
 	XCOPY "./shared/sample-data\*.*" "sample-data" /E /S /I
 	docker cp sample-data sand_market_box_web:/var/www
-	docker exec -it sand_market_box_web chown -R magento2:magento2 /var/www/sample-data
+	docker exec -it sand_market_box_web chown -R magento2:magento2 /home/magento2/magento2-sample-data
 	RMDIR sample-data /S /Q
 	TIMEOUT 5
 
 	ECHO Create symlinks For Sample Data modules, set valid permissions and upgrading database
-	docker-compose exec web php -f /var/www/sample-data/dev/tools/build-sample-data.php -- --ce-source="/var/www/magento2"
-	docker-compose exec web chown -R :magento2 /var/www/sample-data
-	docker-compose exec --user magento2 web sh -c 'find /var/www/sample-data -type d -exec chmod g+ws {} \;'
-	docker-compose exec --user magento2 web rm -rf /var/www/sample-data/cache/* /var/www/sample-data/page_cache/* /var/www/sample-data/generation/*
-	docker-compose exec --user magento2 web php /var/www/magento2/bin/magento setup:upgrade
+	docker-compose exec web php -f /home/magento2/magento2-sample-data/dev/tools/build-sample-data.php -- --ce-source="/home/magento2/magento2"
+	docker-compose exec web chown -R :magento2 /home/magento2/magento2-sample-data
+	docker-compose exec --user magento2 web sh -c 'find /home/magento2/magento2-sample-data -type d -exec chmod g+ws {} \;'
+	docker-compose exec --user magento2 web rm -rf /home/magento2/magento2/cache/* /home/magento2/magento2/page_cache/* /home/magento2/magento2/generation/*
+	docker-compose exec --user magento2 web php /home/magento2/magento2/bin/magento setup:upgrade
 	
 	ECHO Reindexing (this can take a while)
 	docker-compose exec --user magento2 web rm -rf magento2/var
-	docker-compose exec --user magento2 web php /var/www/magento2/bin/magento indexer:reindex
+	docker-compose exec --user magento2 web php /home/magento2/magento2/bin/magento indexer:reindex
 
 	GOTO :end
 )
